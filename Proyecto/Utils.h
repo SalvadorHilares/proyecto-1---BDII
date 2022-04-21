4c14a2dfa80a2fece18ef4871cca16c31e6006db
Claudio Echarre <48997991+cecharre@users.noreply.github.com>
2022-04-21 16:49:51 -0500

    add Utils

---
+++ Proyecto/Utils.h
@@ -0,0 +1,12 @@
+#ifndef PROYECTO1_BD2_LIBRERIAS_H
+#define PROYECTO1_BD2_LIBRERIAS_H
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#endif //PROYECTO1_BD2_LIBRERIAS_H
